6e6286dd5c7130f35affe4822d2aafdcdde95fe0
task-create <seanf@trentonsoupkitchen.org>
2025-10-12 06:39:21 -0400

    Update trainings.js

--- api/trainings.js
+++ api/trainings.js
@@ -1,6 +1,5 @@
 // /api/trainings.js
 import { createClient } from '@supabase/supabase-js';
-
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 
 export default async function handler(req, res) {
